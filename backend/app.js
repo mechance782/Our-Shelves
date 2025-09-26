5b15dbe8cee5f75e3d42d4bb8cf7b2871e816b4f
alstondsouza1 <alstondsouza35@gmail.com>
2025-09-25 22:31:04 -0700

    setup MySQL connection, added .env, created .env example for partner to setup

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import db from "./db.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
+app.get('/', (req, res) => res.send('Hello World'));
 
+app.get("/db-test", async (_, res) => {
+    try {
+      const [rows] = await db.query("SELECT NOW() AS now");
+      res.json({ message: "Database connected!", time: rows[0].now });
+    } catch (error) {
+      console.error("Database error:", error);
+      res.status(500).json({ error: "Database connection failed" });
+    }
+  });  
 
-app.get('/', (req,res) => {
-    res.send("Hello World");
-})
-
-app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
-
-
+app.listen(PORT, () => 
+    console.log(`Server running at http://localhost:${PORT}`)
+);
