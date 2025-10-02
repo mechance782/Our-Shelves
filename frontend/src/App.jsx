c93f50d4b7cce0e67db948464b12c5de83ad1a1c
alstondsouza1 <alstondsouza35@gmail.com>
2025-10-02 10:20:10 -0700

    fixed a error, undo my changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,14 @@
-import { NavLink, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Search from "./pages/Search.jsx";
+import { useState } from 'react'
+import './App.css'
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div style={{ fontFamily: "system-ui", padding: 20 }}>
-      <h1>Our Shelves</h1>
-      <nav style={{ display: "flex", gap: 12, marginBottom: 20 }}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/search">Search</NavLink>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </div>
-  );
+   <>
+   <h1>Hello World</h1>
+   </>
+  )
 }
+
+export default App
