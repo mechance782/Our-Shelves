02a5ceabc49142ffbdc8f7d2afd4d749f54d03b4
alstondsouza1 <alstondsouza35@gmail.com>
2025-10-08 01:58:57 -0700

    made small changes with ecosytem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,26 +4,28 @@ module.exports = {
       name: "backend",
       cwd: "./backend",
       script: "npm",
-      args: "run dev",   
+      args: "run dev", 
       env: {
         NODE_ENV: "development",
         PORT: 3000
       },
       env_production: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 3000
       }
     },
     {
       name: "frontend",
       cwd: "./frontend",
       script: "npm",
-      args: "run dev -- --host 0.0.0.0",   
+      args: "run dev -- --host 0.0.0.0",  
       env: {
         NODE_ENV: "development",
         PORT: 5173
       },
       env_production: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 5173
       }
     }
   ]
