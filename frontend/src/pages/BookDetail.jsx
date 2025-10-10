7fbbc7c6a915e311f2f23729d800238cb6129a5a
dannymccarragher <dannymccarragher@gmail.com>
2025-10-10 11:30:42 -0700

    POST req to backend for adding book to db

--- frontend/src/pages/BookDetail.jsx
+++ frontend/src/pages/BookDetail.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 
 const BookDetail = ({ book }) => {
-    return (
-        <div>
-            <h1>{book.title}</h1>
-            <p>Author: {book.author}</p>
-            {book.Reyear && <p>Year: {book.Reyear}</p>}
-            {book.cover && <img src={book.cover} alt={book.title} />}
-        </div>
-    );
+
+  const addToLibrary = async (book) => {
+    try {
+      const URL = `http://localhost:3000/books`;
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      };
+
+      const response = await fetch(URL, options);
+
+      if (!response.ok) {
+        throw new Error(`Server error: ${response.status}`);
+      }
+
+      alert(`${book.title} added to your library!`);
+
+    } catch (error) {
+      console.error("Error adding book:", error);
+      alert("Failed to add book.");
+    }
+  };
+
+  return (
+    <div className="book-details">
+      <h1>{book.title}</h1>
+      <p>Author: {book.author}</p>
+      {book.Reyear && <p>Year: {book.Reyear}</p>}
+      {book.cover && <img src={book.cover} alt={book.title} />}
+
+      <button id="add-book-button" onClick={() => addToLibrary(book)}>
+        Add book to library
+      </button>
+    </div>
+  );
 };
 
 export default BookDetail;
